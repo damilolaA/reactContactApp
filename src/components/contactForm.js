46762cfd0b007cc3feabf9fa5bcb1354f5b44773
dammy <adiodamilola@yahoo.com>
2018-02-08 02:12:54 +0100

    define state for contactForm component

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -3,14 +3,28 @@ import React, { Component } from "react";
 
 class contactForm extends Component {
 
+	constructor() {
+		super()
+		this.state = {
+			contactDetails: {}
+		}
+	}
+
+	manageSubmit(e) {
+		if(this.ref.fname.value === "" || this.ref.fname.value === "") {
+			
+		}
+		e.preventDefault();
+	}
+
 	render() {
 		
 		return (
 			<div className="App">
-				<form id="add-contact" className="module-active">
-					<label for="add-contact" className="header">Add Contact</label>
-					<input type="text" className="contact-name" name="fname" placeholder="Name"/>
-					<input type="text" className="phone-number" name="number" placeholder="Phone number"/>   
+				<form id="add-contact" className="module-active" onSubmit={this.manageSubmit.bind(this)}>
+					<label htmlFor="add-contact" className="header">Add Contact</label>
+					<input type="text" className="contact-name" ref="fname" placeholder="Name"/>
+					<input type="text" className="phone-number" ref="number" placeholder="Phone number"/>   
 					<input type="submit" value="create" className="submit"/>
 				</form>
 			</div>
